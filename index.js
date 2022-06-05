0b08ccddb3d2c2af5044a90ac013e7c5a3a3fc92
mohammadliazul <liazulislam0@gmail.com>
2022-06-06 00:20:08 +0600

    post and get operation implement (products)

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const port = process.env.PORT || 5000
-
+const port = process.env.PORT || 5000;
+// middleware
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hta0c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -19,7 +20,31 @@ async function run() {
     try {
         await client.connect();
         // check db connection paisee cmd te
-        console.log('database connected successfully');
+        console.log('Database connected successfully');
+
+        const database = client.db("hekto_Database");
+        const productsCollection = database.collection("products");
+
+
+        // POST API
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            // console.log('hit the post api', product);
+            // res.send('post hitted');
+
+            const result = await productsCollection.insertOne(product);
+            // console.log(result);
+            res.json(result);
+        });
+
+        // Get Products API
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+
     }
     finally {
         // await client.close();
@@ -28,7 +53,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello Heiko Server!')
+    res.send('Running Hekto Server')
 })
 
 app.listen(port, () => {
