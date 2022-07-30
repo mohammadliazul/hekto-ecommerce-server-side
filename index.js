b0cc16b7431a0ce900989b5103f7e3e7bbcb2af8
mohammadliazul <liazulislam0@gmail.com>
2022-07-30 08:34:10 +0600

    manage order, order-status

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const admin = require("firebase-admin");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const e = require('express');
 
 const port = process.env.PORT || 5000;
+
+// 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -16,6 +26,23 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 // check uri paisee cmd te 
 // console.log(uri);
 
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        let token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+
+    }
+    next();
+}
+
+
 async function run() {
     try {
         await client.connect();
@@ -25,6 +52,9 @@ async function run() {
         const database = client.db("hekto_Database");
         const productsCollection = database.collection("products");
         const reviewsCollection = database.collection("reviews");
+        const usersCollection = database.collection("users");
+        const ordersCollection = database.collection("orders");
+   
 
 
         // POST products API
@@ -45,6 +75,72 @@ async function run() {
             res.send(products);
         });
 
+        // Delete Product API
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log('getting specific product id', id);
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.json(result);
+        });
+
+        
+
+        // Admin yes or not API
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
+
+
+        // POST users API
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // console.log('hit the post user api', user);
+            // res.send('post user hitted');
+
+            const result = await usersCollection.insertOne(user);
+            // console.log(result);
+            res.json(result);
+        });
+
+        // PUT users API
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        // Add admin role
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            // console.log('put req.headers >>>', req.headers);
+            // console.log('put req.headers.authorization >>>', req.headers.authorization);
+            const requester = req.decodedEmail;
+            // console.log('put requester >>>', requester);
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.send(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'You do not have access to make admin' });
+            }
+        })
+
         // POST reviews/testimonials API
         app.post('/reviews', async (req, res) => {
             const review = req.body;
@@ -63,6 +159,112 @@ async function run() {
             const reviews = await cursor.toArray();
             res.send(reviews);
         });
+        
+        // Get purchase product id api
+        app.get('/purchase/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log('getting specific product', id);
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.findOne(query);
+            res.json(result);
+        });
+
+        // Order Now
+        app.put("/orders", async (req, res) => {
+            const order = req.body;
+            const existUserEmail = await ordersCollection.findOne({ email: order.email });
+            const existUserProductId = await ordersCollection.findOne({ product_id: order.product_id });
+            const query = { email: order.email, product_id: order.product_id };
+            const exist = await ordersCollection.findOne(query);
+
+            if (existUserEmail === null || existUserProductId === null || exist == null) {
+                // console.log('if condition working')
+                const result = await ordersCollection.insertOne(order);
+                res.json(result);
+            }
+            else {
+                // console.log('else condition working');
+                const filter = { exist };
+                const options = { upsert: true }
+                const updateDoc = {
+                    $set: {
+                        exist: order?.exist,
+                        displayName: order.displayName,
+                        email: order?.email,
+                        quantity: exist?.quantity + order?.quantity,
+                        price: order?.price * (exist?.quantity + order?.quantity),
+                        product_id: order?.product_id,
+                        status: order?.status,
+                        thumbnail: order?.thumbnail,
+                        title: order?.title,
+
+                    }
+                };
+                const result = await ordersCollection.updateOne(filter, updateDoc, options);
+                const delete_Exist = ordersCollection.deleteOne(exist);
+                res.json(result);
+            }
+        })
+
+        // Get orders API
+        app.get('/orders/:email', async (req, res) => {
+            const result = await ordersCollection.find({ email: req.params.email }).toArray();
+            // console.log(result);
+            res.send(result);
+        });
+
+        // user delete order api (before user place order) and
+        // manage order delete api
+        app.delete('/order/delete/:id', async (req, res) => {
+            const deleteOrderId = req.params.id;
+            // console.log("deleteOrderId >>>", deleteOrderId);
+            const result = await ordersCollection.deleteOne({ _id: ObjectId(deleteOrderId) });
+            // console.log(result);
+            res.json(result);
+        });
+
+        app.put("/orders-placed/:email", async (req, res) => {
+        const shippingInfo = req.body;
+        const email = req.params.email;
+        // console.log(shippingInfo, email);
+        const filter = { email: email };
+        // const options = { upsert: false }
+        const updateDoc = { 
+            $set: {
+                shippingName: shippingInfo?.shippingName,
+                shippingEmail: shippingInfo?.shippingEmail,
+                address: shippingInfo?.address,
+                apartment: shippingInfo?.apartment,
+                city: shippingInfo?.city,
+                postcode: shippingInfo?.postcode,
+                country: shippingInfo?.country,
+                phone: shippingInfo?.phone,
+            }
+        };
+        const result = await ordersCollection.updateMany(filter, updateDoc);
+        res.send(result);
+        });
+
+        // Manage orders get API
+        app.get("/manage-orders", async (req, res) => {
+        const result = await ordersCollection.find({}).toArray();
+        res.send(result);
+        });
+
+        // update order status
+        app.put("/update-order-status/:id", (req, res) => {
+            const id = req.params.id;
+            const updatedStatus = req.body.status;
+            const filter = { _id: ObjectId(id) };
+            // console.log(updatedStatus);
+            ordersCollection
+                .updateOne(filter, {
+                    $set: { status: updatedStatus },
+                })
+                .then((result) => {
+                    res.send(result);
+                });
+        });
 
     }
     finally {
@@ -72,9 +274,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Running Hekto Server')
+    res.send('Running Hekto Server');
 })
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
+    console.log(`listening on port ${port}`);
 })
